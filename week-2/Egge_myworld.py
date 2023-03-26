3b9eaee5a489ffcc23aaef0f659a03d2ae8e2032
First Last <FL@academind.com>
2023-03-26 19:28:34 -0400

    week-2

---
+++ week-2/Egge_myworld.py
@@ -0,0 +1,11 @@
+"""
+Title: Egge_myworld
+Written by: William Egge
+Description: A python document that serves as "Hello World" for this assignment 
+Date: 2023-03-25
+
+This module contains several functions that perform various tasks.
+
+"""
+
+print("You are now in Egge’s world!")
